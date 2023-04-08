d5e9db8cf5d1039667253c876871561e7773c9c3
yuchen <116358@qq>
2023-04-09 01:58:23 +0800

    Add balance k param auto choose.

--- rm_chassis_controllers/include/rm_chassis_controllers/balance.h
+++ rm_chassis_controllers/include/rm_chassis_controllers/balance.h
@@ -8,6 +8,7 @@
 #include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/imu_sensor_interface.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <rm_common/hardware_interface/rm_imu_sensor_interface.h>
 #include <rm_chassis_controllers/QRConfig.h>
 #include <rm_msgs/BalanceState.h>
 
@@ -16,7 +17,7 @@
 namespace rm_chassis_controllers
 {
 using Eigen::Matrix;
-class BalanceController : public ChassisBase<rm_control::RobotStateInterface, hardware_interface::ImuSensorInterface,
+class BalanceController : public ChassisBase<rm_control::RobotStateInterface, rm_control::RmImuSensorInterface,
                                              hardware_interface::EffortJointInterface>
 {
   enum BalanceMode
@@ -34,16 +35,18 @@ private:
   void moveJoint(const ros::Time& time, const ros::Duration& period) override;
   void normal(const ros::Time& time, const ros::Duration& period);
   void block(const ros::Time& time, const ros::Duration& period);
-  void fallen(const ros::Time& time, const ros::Duration& period);
+
   void publishState(const ros::Time& time);
   void reconfigCB(rm_chassis_controllers::QRConfig& config, uint32_t /*level*/);
   geometry_msgs::Twist odometry() override;
   static const int STATE_DIM = 10;
   static const int CONTROL_DIM = 4;
-  Eigen::Matrix<double, CONTROL_DIM, STATE_DIM> k_{};
-  Eigen::Matrix<double, STATE_DIM, STATE_DIM> a_{}, q_{};
+  void getQ(const XmlRpc::XmlRpcValue& q, Eigen::Matrix<double, STATE_DIM, STATE_DIM>& eigen_q);
+  void getR(const XmlRpc::XmlRpcValue& r, Eigen::Matrix<double, CONTROL_DIM, CONTROL_DIM>& eigen_r);
+  Eigen::Matrix<double, CONTROL_DIM, STATE_DIM> k_mid_{}, k_fallen_{}, k_low_{}, k_high_{};
+  Eigen::Matrix<double, STATE_DIM, STATE_DIM> a_{}, q_mid_{}, q_fallen_{}, q_low_{}, q_high_{};
   Eigen::Matrix<double, STATE_DIM, CONTROL_DIM> b_{};
-  Eigen::Matrix<double, CONTROL_DIM, CONTROL_DIM> r_{};
+  Eigen::Matrix<double, CONTROL_DIM, CONTROL_DIM> r_mid_{}, r_low_{}, r_high_{};
   Eigen::Matrix<double, STATE_DIM, 1> x_;
   double wheel_radius_, wheel_base_;
   double position_des_ = 0;
@@ -61,9 +64,9 @@ private:
   ros::Time block_time_, last_block_time_;
   double fallen_angle_, block_angle_, block_duration_, block_velocity_, block_effort_, anti_block_effort_,
       block_overtime_;
-  bool balance_state_changed_ = false, maybe_block_ = false;
+  bool balance_state_changed_ = false, maybe_block_ = false, imu_online_;
 
-  hardware_interface::ImuSensorHandle imu_handle_;
+  rm_control::RmImuSensorHandle imu_handle_;
   hardware_interface::JointHandle left_wheel_joint_handle_, right_wheel_joint_handle_,
       left_momentum_block_joint_handle_, right_momentum_block_joint_handle_;
 
